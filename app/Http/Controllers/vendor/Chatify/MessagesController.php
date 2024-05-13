2779ff984ddfa2e0ca13fb5c1a98e2324a22813e
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2024-05-13 00:10:21 -0400

    En fin

--- app/Http/Controllers/vendor/Chatify/MessagesController.php
+++ app/Http/Controllers/vendor/Chatify/MessagesController.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request as FacadesRequest;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 
 class MessagesController extends Controller
 {
